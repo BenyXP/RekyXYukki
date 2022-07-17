f06576759b42a93eab1322379e10ddb975347e30
BenyXP <103335260+BenyXP@users.noreply.github.com>
2022-07-17 08:06:27 +0700

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -20,11 +20,10 @@ class YukkiBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            name="AikaBot",
+            "YukkiMusicBot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            in_memory=True,
         )
 
     async def start(self):
@@ -46,6 +45,7 @@ class YukkiBot(Client):
             LOGGER(__name__).error(
                 "Please promote Bot as Admin in Logger Group"
             )
+            sys.exit()
         if get_me.last_name:
             self.name = get_me.first_name + " " + get_me.last_name
         else:
